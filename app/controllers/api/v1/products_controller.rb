1f3a4d5af8ef5160014e4a9f094ed682d2bd9352
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2023-03-17 18:17:23 -0500

    Added initial controller - Products controller with index endpoint

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,8 @@
+module Api::V1
+  class ProductsController < ApplicationController
+
+    def index
+      render json: {}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  scope module: 'api' do
+    namespace :v1 do
+      get '/products', to: 'products#index'
+    end
+  end
 end
